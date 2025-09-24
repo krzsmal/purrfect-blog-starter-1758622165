97fd2749171125725655116d57159c3f928798ab
Krzysztof Smal <krz.smal@gmail.com>
2025-09-24 13:11:56 +0200

    feat(blog): add BlogPostController with CRUD actions
    - Create BlogPostController with Index, Details, CreatePost actions
    - Add GET and POST CreatePost methods with validation
    - Configure custom /CreatePost route in RouteConfig
    - Add database context disposal in controller
    - Initialize database in Application_Start

--- PurrfectBlog/Global.asax.cs
+++ PurrfectBlog/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using PurrfectBlog.Models;
+
 
 namespace PurrfectBlog
 {
@@ -16,6 +19,9 @@ namespace PurrfectBlog
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize database
+            Database.SetInitializer(new CreateDatabaseIfNotExists<BlogDbContext>());
         }
     }
 }
